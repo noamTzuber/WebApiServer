ca2d2c1d044b87a2a66fa9fa7a3c82c7ddf62e24
itayshwartz1 <itay2007@gmail.com>
2022-05-22 10:25:46 +0300

    aa

---
+++ noam2/Hub/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chatty.Api.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
